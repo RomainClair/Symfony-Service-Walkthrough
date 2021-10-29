269ca7fc8a6259f08319e737ad50e5f3f545c927
Romain Clair <romain.clair@wildcodeschool.fr>
2021-10-29 12:27:02 +0200

    Add some non functionnal example code

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use App\Repository\SpecialOfferRepository;
 use App\Repository\UserRepository;
 use App\Service\PriceCalculator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,6 +24,21 @@ class ProductController extends AbstractController
             'product' => $product,
             'price' => $price,
         ]);
+    }
+
+    /**
+     * @Route("/add", name="add-product)
+     */
+    public function add(EntityManagerInterface $manager): Response
+    {
+        // Creating a new product
+        $product = new Product();
+        // Initialiazing the product, for example using a form
+        // ...
+        // Save the product
+        $manager->persist($product);
+        $manager->flush();
+        // Generate a response, for example a redirection
     }
 
     // Redefine getUser for test purposes
